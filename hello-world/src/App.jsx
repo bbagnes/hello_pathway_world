b762e5ec171e62d9016703ab86bfd20fc9dfdea6
bbagnes <bbagnes@byupathway.edu>
2026-03-06 14:23:14 -0700

    first commit

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        <h1>
+          Hello BYU Pathway World!
+        </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
